3f9c8225ad4d890b6445a9c5f1b1a75bcbdd69a6
zehan12 <zehan9211@gmail.com>
2025-03-19 00:10:03 +0530

    feat: :seedling: add user schema and run migration

---
+++ prisma/migrations/20250318210046_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'EDITOR', 'VIEWER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'VIEWER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
