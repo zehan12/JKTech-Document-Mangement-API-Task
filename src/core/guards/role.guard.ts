9fc6e9c2375633ff64139834189d92322773f2bb
zehan12 <zehan9211@gmail.com>
2025-03-19 13:45:32 +0530

    feat: :sparkles: add role guard and decorator

---
+++ src/core/guards/role.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.get<string[]>('roles', context.getHandler());
+        if (!requiredRoles) return true;
+
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+
+        if (!user || !requiredRoles.includes(user.role)) {
+            throw new ForbiddenException('You do not have permission to perform this action');
+        }
+
+        return true;
+    }
+}
