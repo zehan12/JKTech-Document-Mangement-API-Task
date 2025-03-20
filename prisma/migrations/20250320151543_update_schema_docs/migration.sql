dc8c62cb66c2a46d7093d0b3a6e1af00925a75bd
zehan12 <zehan9211@gmail.com>
2025-03-20 12:04:03 +0530

    chore: :seedling: add db migrations for document table

---
+++ prisma/migrations/20250320151543_update_schema_docs/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
